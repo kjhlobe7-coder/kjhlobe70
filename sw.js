17715c76fcfa4511005c5722fed04910b381e03c
kjhlobe7-coder <kjhlobe7-coder@users.noreply.github.com>
2026-04-09 15:33:59 +0900

    feat: add pwa install support for mobile

---
+++ sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = "ksic-pwa-v1";
+const CORE_ASSETS = [
+  "/",
+  "/index.html",
+  "/complaint_router_ui.html",
+  "/manifest.webmanifest",
+  "/ksic_index_full.json",
+  "/icons/icon-192.svg",
+  "/icons/icon-512.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const url = new URL(event.request.url);
+  const isDataRequest = url.pathname.endsWith("/ksic_index_full.json");
+
+  if (isDataRequest) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      });
+    })
+  );
+});
